63c7e863fd3e26829a2b8577436acf9720ed5ad2
alex <emal@damain.com>
2023-07-11 12:33:44 +0200

    feat: add basic next

--- index.js
+++ index.js
@@ -35,11 +35,12 @@ wss.on("connection", (ws) => {
 function dispatchReq(rawRequest, current_connection) {
   const json_object = JSON.parse(rawRequest);
   let res;
+
   console.log("req-->", json_object);
-  const { type, data, id } = json_object;
+
+  const { type, data } = json_object;
   switch (type) {
     case "reg":
-      console.log("reg");
       const { name, password } = JSON.parse(data);
       //create user
       const new_user = {
@@ -49,10 +50,10 @@ function dispatchReq(rawRequest, current_connection) {
         index: Number(current_connection),
         error: false,
         errorText: "",
+        wins: 0,
       };
 
       db.users[current_connection] = structuredClone(new_user);
-      console.log("users-->", db.users);
 
       //reg
       const reg_data = (({ password, ...rest }) => rest)(new_user);
@@ -97,7 +98,8 @@ function dispatchReq(rawRequest, current_connection) {
         board: {},
         turn: !!Math.round(Math.random()) ? String(db.rooms[indexRoom].roomUsers[0].index) : current_connection,
       };
-
+      console.log(`pl1 ${new_game.player_1}   pl2 ${new_game.player_2}`);
+      // console.log("turn-->", new_game.turn);
       db.games.push(new_game);
 
       //clean room
@@ -109,6 +111,7 @@ function dispatchReq(rawRequest, current_connection) {
         data: { idGame: new_game.idGame, idPlayer: Number(new_game.player_2) },
         id: 0,
       };
+      console.log(`cr game cur${current_connection}  op ${new_game.player_2}`);
       const res_1 = { ...new_game_res, data: JSON.stringify(new_game_res.data) };
       sendToOne(res_1, new_game.player_1);
       let res_2 = { ...new_game_res };
@@ -117,66 +120,55 @@ function dispatchReq(rawRequest, current_connection) {
       sendToOne(res_2, new_game.player_2);
 
       break;
+
     case "add_ships":
       const { gameId, ships, indexPlayer } = JSON.parse(data);
-      const enemy_id = indexPlayer;
+      const enemy_id = db.games[gameId].player_1 === String(indexPlayer) ? Number(db.games[gameId].player_2) : Number(db.games[gameId].player_1);
+
       // set ships
       db.games[gameId].board[current_connection] = {};
       db.games[gameId].board[current_connection].ships = ships;
       db.games[gameId].board[current_connection].enemy = enemy_id;
       db.games[gameId].ready = db.games[gameId].ready + 1;
+
       //set fields
       setField(current_connection, gameId, ships);
+
       if (db.games[gameId].ready === 2) {
         for (const key in db.games[gameId].board) {
           const res = {
             type: "start_game",
             data: JSON.stringify({
               ships: db.games[gameId].board[key].ships,
-              currentPlayerIndex: Number(enemy_id), //????
+              currentPlayerIndex: Number(enemy_id),
             }),
             id: 0,
           };
           sendToOne(res, key);
         }
-
-        //turn
         sendTurn(gameId);
       }
       break;
+
     case "attack":
       const { x, y, gameId: game_id, indexPlayer: index_player } = JSON.parse(data);
       if (index_player !== Number(db.games[game_id].turn)) {
         return;
       }
-      //check
-      const coords = checkShot(data);
-      //
-      coords.forEach((coord) => {
-        const atttac_res = {
-          type: "attack",
-          data: JSON.stringify({
-            position: {
-              x: coord[0],
-              y: coord[1],
-            },
-            currentPlayer: index_player,
-            status: coord[2],
-          }),
-          id: 0,
-        };
 
-        sendToAllLogged(atttac_res);
-      });
+      const coords = checkShot(JSON.parse(data));
+
+      sendShots(coords, index_player);
       sendTurn(game_id);
       break;
 
     case "randomAttack":
-      let { x: r_x, y: r_y, gameId: r_game_id, indexPlayer: r_index_player } = JSON.parse(data);
-      sendTurn(r_game_id);
-      break;
+      const { gameId: r_game_id, indexPlayer: r_index_player } = JSON.parse(data);
+      const prep_data = { ...JSON.parse(data), x: random(), y: random() };
+      const r_coords = checkShot(prep_data);
 
-    default:
+      sendShots(r_coords, r_index_player);
+      sendTurn(r_game_id);
       break;
   }
 }
@@ -185,18 +177,18 @@ function sendToOne(res, current_connection) {
   db.connections[current_connection].send(JSON.stringify(res));
 }
 
-//add che
 function sendToAllLogged(res) {
   for (const key in db.users) {
     db.connections[key].send(JSON.stringify(res));
   }
 }
 
-const genNumdersToken = (length = 15) => {
+function genNumdersToken(length = 10) {
   const base = 10 ** length;
   const token = Math.floor(base + Math.random() * 9 * base).toString(); // token
   return token;
-};
+}
+
 function sendToAllUpatedRooms() {
   const update_room_res = {
     type: "update_room",
@@ -206,23 +198,10 @@ function sendToAllUpatedRooms() {
   sendToAllLogged(update_room_res);
 }
 
-function sendTurn(gameId) {
-  const turn = db.games[gameId].turn === db.games[gameId].player_1 ? db.games[gameId].player_2 : db.games[gameId].player_1;
-  db.games[gameId].turn = turn;
-  const res = {
-    type: "turn",
-    data: JSON.stringify({
-      currentPlayer: Number(turn),
-    }),
-    id: 0,
-  };
-
-  sendToAllLogged(res); //chande to in curr game
-}
-
 function setField(current_connection, gameId, ships) {
   db.games[gameId].board[current_connection].field = ships;
   const field = db.games[gameId].board[current_connection].field;
+  db.games[gameId].board[current_connection].ships_on = 10;
 
   field.forEach((ship, ind) => {
     const coords = [];
@@ -236,7 +215,7 @@ function setField(current_connection, gameId, ships) {
   });
   field.forEach((ship, ind) => {
     field[ind].area = [];
-    console.log("ship-->", ship);
+
     if (ship.direction === false) {
       const s = ship.coords[0];
       field[ind].area.push([s[0] - 1, s[1] - 1], [s[0] - 1, s[1]], [s[0] - 1, s[1] + 1]);
@@ -256,8 +235,9 @@ function setField(current_connection, gameId, ships) {
     }
   });
 }
+
 function checkShot(data) {
-  const { x, y, gameId, indexPlayer } = JSON.parse(data);
+  const { x, y, gameId, indexPlayer } = data;
   const field = db.games[gameId].board[String(indexPlayer)].field;
   let coords = [[x, y, "miss"]];
   field.forEach((ship, idx) => {
@@ -266,25 +246,100 @@ function checkShot(data) {
       const new_coords = ship.coords.filter((el) => !(el[0] === x && el[1] === y));
       field[idx].coords = new_coords;
       const after_sip_size = new_coords.length;
-      console.log(` ${before_ship_size}-+++-${after_sip_size}`);
       if (after_sip_size === 0) {
         //killed
         coords = [];
         field[idx].origin_coords.forEach((el) => {
           el[2] = "killed";
           coords.push(el);
+          db.games[gameId].board[String(indexPlayer)].ships_on -= 1;
+
+          ///have to be 0 !!! win!!!
+          if (db.games[gameId].board[String(indexPlayer)].ships_on <= 0) {
+            const winner_id = String(indexPlayer) === db.games[gameId].player_1 ? db.games[gameId].player_2 : db.games[gameId].player_1;
+
+            db.users[String(winner_id)].wins += 1; //add win to player
+            let wins_table = [];
+            for (const user in db.users) {
+              if (Object.hasOwnProperty.call(db.users, user)) {
+                if (db.users[user].wins > 0) {
+                  wins_table.push({ name: db.users[user].name, wins: db.users[user].wins });
+                }
+              }
+            }
+
+            //send winner
+            const res = {
+              type: "finish",
+              data: JSON.stringify({
+                winPlayer: indexPlayer,
+              }),
+              id: 0,
+            };
+            sendToAllLogged(res);
+
+            const wins_res = {
+              type: "update_winners",
+              data: JSON.stringify(wins_table),
+              id: 0,
+            };
+            sendToAllLogged(wins_res);
+          }
         });
         field[idx].area.forEach((el) => {
           el[2] = "miss";
           coords.push(el);
         });
+        const turn = db.games[gameId].turn === db.games[gameId].player_1 ? db.games[gameId].player_2 : db.games[gameId].player_1;
+        db.games[gameId].turn = turn;
       } else if (before_ship_size - after_sip_size === 1) {
         coords[0][2] = "shot";
       }
     }
   });
-  console.log(coords);
+  if (coords[0][2] !== "miss") {
+    const turn = db.games[gameId].turn === db.games[gameId].player_1 ? db.games[gameId].player_2 : db.games[gameId].player_1;
+    db.games[gameId].turn = turn;
+  }
   return coords;
+}
+
+function sendTurn(gameId) {
+  const turn = db.games[gameId].turn === db.games[gameId].player_1 ? db.games[gameId].player_2 : db.games[gameId].player_1;
+  db.games[gameId].turn = turn;
+  console.log(`turn ->`, db.games[gameId].turn);
+  const res = {
+    type: "turn",
+    data: JSON.stringify({
+      currentPlayer: Number(db.games[gameId].turn),
+    }),
+    id: 0,
+  };
+  sendToAllLogged(res);
+}
+
+function random(min = 0, max = 9) {
+  const rand = Math.floor(Math.random() * (max - min) + min);
+  return rand;
+}
+
+function sendShots(coords, index_player) {
+  coords.forEach((coord) => {
+    const atttac_res = {
+      type: "attack",
+      data: JSON.stringify({
+        position: {
+          x: coord[0],
+          y: coord[1],
+        },
+        currentPlayer: index_player,
+        status: coord[2],
+      }),
+      id: 0,
+    };
+
+    sendToAllLogged(atttac_res);
+  });
 }
 // console.dir(util.inspect(ws, { showHidden: false, depth: false, colors: true }));
 // const res = structuredClone(newUser);
